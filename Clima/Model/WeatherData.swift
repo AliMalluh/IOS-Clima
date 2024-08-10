5ada5e4ccd457835486cef131d6acfdd31ae837c
Ali Malluh <109945441+AliMalluh@users.noreply.github.com>
2024-08-10 18:49:36 +0300

    Add files via upload

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Ali-Malluh on 26/10/2023.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
